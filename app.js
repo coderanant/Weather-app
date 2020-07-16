c42d03a5047158b90402abe40c10c490a64a50bd
coderanant <coderanant@gmail.com>
2020-07-16 17:12:12 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+const encodedAddress = encodeURIComponent(argv.a);
+const key = 'pk.eyJ1IjoiY29kZXJhbmFudCIsImEiOiJja2NuZmg1YjMwYW9qMzNsdXpzdTk1ZHl2In0.MI7vlkkH2LF_pophEsz3jA';
+
+request({
+    url: `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedAddress}.json?access_token=${key}`,
+    json: true
+}, (error, response, body) => {
+    console.log(`Address: ${body.features[0].place_name}`);
+    console.log(`Latitude: ${body.features[0].center[1]}`);
+    console.log(`Longitude: ${body.features[0].center[0]}`);
+})
