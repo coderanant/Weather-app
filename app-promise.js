b3b2fc856ffa299afeef264cb074c18d92270bda
coderanant <coderanant@gmail.com>
2020-07-18 20:56:09 +0530

    implemented using promises

---
+++ app-promise.js
@@ -0,0 +1,43 @@
+const yargs = require('yargs');
+const axios = require('axios');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+const key = 'pk.eyJ1IjoiY29kZXJhbmFudCIsImEiOiJja2NuZmg1YjMwYW9qMzNsdXpzdTk1ZHl2In0.MI7vlkkH2LF_pophEsz3jA';
+var geocodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedAddress}.json?access_token=${key}`;
+var weatherKey = 'd6353b2124ffab53d8222675f5c6d7ec';
+
+axios.get(geocodeUrl).then((response) => {
+    if (response.data.features.length === 0) {
+        throw new Error('Unable to find the addess.');
+    }
+    var lat = response.data.features[0].center[1];
+    var lng = response.data.features[0].center[0];
+    var weatherUrl = `https://api.darksky.net/forecast/${weatherKey}/${lat},${lng}`
+    console.log(response.data.features[0].place_name);
+    return axios.get(weatherUrl);
+}).then((response) => {
+    var temparature = response.data.currently.temperature;
+    var apparentTemparature = response.data.currently.apparentTemperature;
+
+    console.log(`It's currently ${temparature}. It feels like ${apparentTemparature}.`);
+}).catch((e) => {
+    if(e.code == 'ENOTFOUND') {
+        console.log('Unable to connect to API service.');
+    }
+    else {
+        console.log(e.message);
+    }
+})
